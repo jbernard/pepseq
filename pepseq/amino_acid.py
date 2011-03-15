ef8b3b877b537005ad2fcefb002aeb9c6bebddf2
Jon Bernard <jbernard@tuxion.com>
2011-03-14 22:29:00 -0400

    Add config file parsing and subweights

--- pepseq/amino_acid.py
+++ pepseq/amino_acid.py
@@ -6,27 +6,27 @@ class AminoAcid(object):
     """ Amino acids are molecules containing an amine group, a carboxylic acid
         group and a side chain that varies between different amino acids. """
 
-    aminoAcids = {'G': ('Glycine',                  'Gly', 57.05),
-                  'A': ('Alanine',                  'Ala', 71.09),
-                  'S': ('Serine',                   'Ser', 87.08),
-                  'T': ('Threonine',                'Thr', 101.11),
-                  'C': ('Cysteine',                 'Cys', 103.15),
-                  'V': ('Valine',                   'Val', 99.14),
-                  'L': ('Leucine',                  'Leu', 113.16),
-                  'I': ('Isoleucine',               'Ile', 113.16),
-                  'M': ('Methionine',               'Met', 131.19),
-                  'P': ('Proline',                  'Pro', 97.12),
-                  'F': ('Phenylalanine',            'Phe', 147.18),
-                  'Y': ('Tyrosine',                 'Tyr', 163.18),
-                  'W': ('Tryptophan',               'Trp', 186.21),
-                  'D': ('Aspartic Acid',            'Asp', 115.09),
-                  'E': ('Glutamic Acid',            'Glu', 129.12),
-                  'N': ('Asparagine',               'Asn', 114.11),
-                  'Q': ('Glutamine',                'Gln', 128.14),
-                  'H': ('Histidine',                'His', 137.14),
-                  'K': ('Lysine',                   'Lys', 128.17),
-                  'R': ('Arginine',                 'Arg', 156.19),
-                  'B': ('Borono Phenylalanine',     'Bpa', 309.08),
+    aminoAcids = {'G': ('Glycine',                  'Gly', 57.00),
+                  'A': ('Alanine',                  'Ala', 71.00),
+                  'S': ('Serine',                   'Ser', 87.00),
+                  'T': ('Threonine',                'Thr', 101.00),
+                  'C': ('Cysteine',                 'Cys', 103.00),
+                  'V': ('Valine',                   'Val', 99.10),
+                  'L': ('Leucine',                  'Leu', 113.10),
+                  'I': ('Isoleucine',               'Ile', 113.10),
+                  'M': ('Methionine',               'Met', 131.00),
+                  'P': ('Proline',                  'Pro', 97.10),
+                  'F': ('Phenylalanine',            'Phe', 147.10),
+                  'Y': ('Tyrosine',                 'Tyr', 163.10),
+                  'W': ('Tryptophan',               'Trp', 186.10),
+                  'D': ('Aspartic Acid',            'Asp', 115.00),
+                  'E': ('Glutamic Acid',            'Glu', 129.00),
+                  'N': ('Asparagine',               'Asn', 114.00),
+                  'Q': ('Glutamine',                'Gln', 128.10),
+                  'H': ('Histidine',                'His', 137.10),
+                  'K': ('Lysine',                   'Lys', 128.10),
+                  'R': ('Arginine',                 'Arg', 156.10),
+                  'B': ('Borono Phenylalanine',     'Bpa', 394.10),
     }
 
     def __init__(self, letter):
@@ -45,19 +45,34 @@ class Peptide(object):
     def __init__(self, amino_acids):
         self.amino_acids = amino_acids
         self.weight = 0
+        self.subweights = []
         self.string = ""
         for amino_acid in amino_acids:
             self.weight += amino_acid.weight
             self.string += ('%s' % (amino_acid.letter))
 
     def branch_before(self, index):
-        self.weight = 0
+        self.weight = 18.00
         for (counter, amino_acid) in enumerate(self.amino_acids):
             if counter < index:
                 self.weight += 2 * amino_acid.weight
             else:
                 self.weight += amino_acid.weight
-        self.string = '(' + self.string[0:index] + ')2' + self.string[index:]
+        self.string = ('(' + self.string[0:index] + ')2 ' +
+                       self.string[index + 1:])
+        self._calculate_subweights()
+
+    def _calculate_subweights(self):
+
+        self.subweights.append(str(self.weight - self.amino_acids[0].weight))
+        self.subweights.append(str(float(self.subweights[0]) - self.amino_acids[1].weight))
+        self.subweights.append(str(float(self.subweights[1]) - self.amino_acids[2].weight))
+        self.subweights.append(str(float(self.subweights[2]) - self.amino_acids[3].weight))
+
+        self.subweights.append(str(self.weight - 17.1 - self.amino_acids[-1].weight))
+        self.subweights.append(str(float(self.subweights[-1]) - self.amino_acids[-2].weight))
+        self.subweights.append(str(float(self.subweights[-1]) - self.amino_acids[-3].weight))
+        self.subweights.append(str(float(self.subweights[-1]) - self.amino_acids[-4].weight))
 
     def __cmp__(self, other):
         return cmp(self.weight, other.weight)
@@ -70,7 +85,7 @@ if __name__ == '__main__':
 
     import itertools
 
-    LOW  = 2636.0
+    LOW = 2636.0
     HIGH = 2637.0
 
     possibilities = (
